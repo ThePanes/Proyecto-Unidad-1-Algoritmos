807c883766d48dcb8b8d54680d371a47e5a337be
Oscar Montecinos <oscar.montecinos.guerrero@gmail.com>
2023-10-02 21:19:53 -0300

    creacion estructura basica

---
+++ proyectoUnidad1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Carta{
+    char nombreCarta[50];
+    char tipoCarta[50];
+    int puntosVida;
+    int puntosDefensa;
+    int puntosAtaque;
+};
+
+int main(){
+
+
+
+
+
+
+
+
+    return 0;
+}
