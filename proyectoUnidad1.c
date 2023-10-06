7ba234f9e4a73f726201669ea7bebf8ae3714f34
Oscar Montecinos <oscar.montecinos.guerrero@gmail.com>
2023-10-05 23:42:03 -0300

    Agregar Cosas

--- proyectoUnidad1.c
+++ proyectoUnidad1.c
@@ -59,14 +59,45 @@ int main(){
     Carta *primeraCarta = NULL;
     Carta *nuevaCarta;
 
+    int opcion;
+    char nom[50];
+    char tipo[50];
+    int vida, defensa,ataque;
+
     nuevaCarta = crear_Cartita_Mazo("Alan","Mago",10,10,10);
     add_Cartita_Mazo(&primeraCarta, nuevaCarta);
 
     nuevaCarta = crear_Cartita_Mazo("Matias","Brujo",10,10,10);
     add_Cartita_Mazo(&primeraCarta, nuevaCarta);
-
+    
     imprimeCartas(primeraCarta);
+    
+    printf("\nBienvenido, que desea hacer?\n");
+    printf("\1.nueva carta en mazo");
+    scanf("%d",&opcion);
+    switch(opcion){
 
+        case 1:
+            printf("\nIngrese nombre de la carta\n");
+            scanf("%s",&nom);
+            printf("\nIngrese tipo de la carta\n");
+            scanf("%s",&tipo);
+            printf("\nIngrese vida de la carta\n");
+            scanf("%d",&vida);
+            printf("\nIngrese defensa de la carta\n");
+            scanf("%d",&defensa);
+            printf("\nIngrese ataque de la carta\n");
+            scanf("%d",&ataque);
+            nuevaCarta = crear_Cartita_Mazo(nom,tipo,vida,defensa,ataque);
+            add_Cartita_Mazo(&primeraCarta, nuevaCarta);
+            break;
+
+        default: break;
+    }
+
+
+
+    
 
     return 0;
 }
